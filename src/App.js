785ae3bd66cd1605008b4278267339cd1d36e8e3
HaibulHasan <habibul.hasan.hira@gmail.com>
2021-09-18 21:47:42 +0600

    Added calender ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { Container, Row } from "react-bootstrap";
+import "../node_modules/react-big-calendar/lib/css/react-big-calendar.css";
 import Calender from "./components/calender";
 import Nav from "./Nav";
 const App = () => (
--- src/components/calender/index.js
+++ src/components/calender/index.js
@@ -1,8 +1,15 @@
 import axios from "axios";
+import moment from "moment";
 import React, { useEffect, useState } from "react";
+import { Calendar, momentLocalizer, Views } from "react-big-calendar";
+
 function Calender() {
+  let allViews = Object.keys(Views).map((k) => Views[k]);
+
+  const localizer = momentLocalizer(moment);
   const [count, setCount] = useState(0);
   const [events, setEvents] = useState([]);
+  console.log(Views);
   useEffect(() => {
     const result = axios.get(
       "https://api.wordpress.org/events/1.0/?location=Seattle"
@@ -15,13 +22,38 @@ function Calender() {
   return (
     <div>
       <p>All events around...</p>
-      <p>
-        {events && events.length > 0
-          ? events.length
-          : "no events till now in this location"}
-      </p>
+      {/* <p>
+        {events && events.length > 0 ? `${JSON.stringify(events[0])}` : "No"}
+      </p> */}
+      <Calendar
+        localizer={localizer}
+        events={[
+          {
+            id: 0,
+            title: "All Day Event very long title",
+            allDay: true,
+            start: new Date(2012, 3, 0),
+            end: new Date(2021, 3, 1),
+          },
+          {
+            id: 1,
+            title: "Word camp",
+            start: new Date(),
+            end: new Date(2021, 11, 10),
+          },
+        ]}
+        defaultDate={new Date(2015, 3, 1)}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        views={allViews}
+        step={6}
+        showMultiDayTimes
+      />
     </div>
   );
 }
 
 export default Calender;
+
+const localizer = momentLocalizer(moment);
