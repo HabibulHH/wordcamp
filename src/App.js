4a3bc02d5976a9a56a84f75130ff364d5cd5a435
HaibulHasan <habibul.hasan.hira@gmail.com>
2021-09-18 12:30:41 +0600

    Added basic styling

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import Nav from "./Nav";
+
+const App = () => (
+  <>
+    <Nav />
+    <Container>
+      <Row></Row>
+    </Container>
+  </>
+);
+
+export default App;
