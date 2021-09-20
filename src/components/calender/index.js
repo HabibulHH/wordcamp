041ae6c16eea30a57a400cb763c4db2334d4c98e
HaibulHasan <habibul.hasan.hira@gmail.com>
2021-09-20 15:58:47 +0600

    Added cutom apia calls to fetch wp word camps

--- src/components/calender/index.js
+++ src/components/calender/index.js
@@ -2,58 +2,106 @@ import axios from "axios";
 import moment from "moment";
 import React, { useEffect, useState } from "react";
 import { Calendar, momentLocalizer, Views } from "react-big-calendar";
+import { Spinner } from "react-bootstrap";
 
 function Calender() {
   let allViews = Object.keys(Views).map((k) => Views[k]);
-
   const localizer = momentLocalizer(moment);
-  const [count, setCount] = useState(0);
   const [events, setEvents] = useState([]);
-  console.log(Views);
+
   useEffect(() => {
     const result = axios.get(
-      "https://api.wordpress.org/events/1.0/?location=Seattle"
+      `https://central.wordcamp.org/?rest_route=/wp/v2/wordcamps&_embed&per_page=10&page=${1}`
     );
-    result.then(({ data }) => {
-      console.log(data.event);
-      setEvents(data.events);
+
+    result.then((res) => {
+      console.log(res.data);
+      let _processedData = [];
+      res.data.map((data) =>
+        _processedData.push({
+          title: data.title.rendered,
+          start: moment.unix(data["Start Date (YYYY-mm-dd)"]).format("llll"),
+          end: moment.unix(data["End Date (YYYY-mm-dd)"]).format("llll"),
+          id: data.id,
+          location: data.location,
+          url: data.URL,
+        })
+      );
+      setEvents(_processedData);
+      let count = res.headers["x-wp-totalpages"];
+      count = count / 10;
+      console.log(count);
+      let axiosCalls = [];
+      let processedData = [];
+
+      for (let i = 1; i < count; i++) {
+        axiosCalls.push(
+          axios.get(
+            `https://central.wordcamp.org/?rest_route=/wp/v2/wordcamps&_embed&per_page=100&page=${i}`
+          )
+        );
+      }
+      axios
+        .all(axiosCalls)
+        .then(
+          axios.spread((...responses) => {
+            console.log(responses.length);
+            responses.map((item) => {
+              item.data.map((data) =>
+                processedData.push({
+                  title: data.title.rendered,
+                  start: moment
+                    .unix(data["Start Date (YYYY-mm-dd)"])
+                    .format("llll"),
+                  end: moment
+                    .unix(data["End Date (YYYY-mm-dd)"])
+                    .format("llll"),
+                  id: data.id,
+                  location: data.location,
+                  url: data.URL,
+                })
+              );
+            });
+            setEvents(processedData);
+            console.log("full data loaded");
+          })
+        )
+        .catch((errors) => {
+          console.log(errors);
+        });
     });
-  });
+  }, []);
   return (
     <div>
-      <p>All events around...</p>
-      {/* <p>
-        {events && events.length > 0 ? `${JSON.stringify(events[0])}` : "No"}
-      </p> */}
-      <Calendar
-        localizer={localizer}
-        events={[
-          {
-            id: 0,
-            title: "All Day Event very long title",
-            allDay: true,
-            start: new Date(2012, 3, 0),
-            end: new Date(2021, 3, 1),
-          },
-          {
-            id: 1,
-            title: "Word camp",
-            start: new Date(),
-            end: new Date(2021, 11, 10),
-          },
-        ]}
-        defaultDate={new Date(2015, 3, 1)}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-        views={allViews}
-        step={6}
-        showMultiDayTimes
-      />
+      {events && events.length > 0 ? (
+        ""
+      ) : (
+        <>
+          <Spinner animation="grow" variant="secondary" />
+          <Spinner animation="grow" variant="success" />
+          <Spinner animation="grow" variant="danger" />
+          <Spinner animation="grow" variant="warning" />
+          <Spinner animation="grow" variant="info" />
+          <Spinner animation="grow" variant="light" />
+          <Spinner animation="grow" variant="dark" />
+        </>
+      )}
+
+      <div>
+        <Calendar
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 600 }}
+          views={allViews}
+          step={6}
+          showAllEvents
+          showMultiDayTimes
+        />
+      </div>
     </div>
   );
 }
 
 export default Calender;
-
-const localizer = momentLocalizer(moment);
