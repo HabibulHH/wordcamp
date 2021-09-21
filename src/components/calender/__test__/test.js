1fea79513504360b6579ef25733c6addc4d59ec4
HaibulHasan <habibul.hasan.hira@gmail.com>
2021-09-21 10:53:38 +0600

    Test codes added for calender and nav bar

--- src/components/calender/__test__/test.js
+++ src/components/calender/__test__/test.js
@@ -1,10 +1,74 @@
+import { cleanup, render } from "@testing-library/react";
+import axios from "axios";
+import React from "react";
+import { unmountComponentAtNode } from "react-dom";
+import Calender from "../";
+// user.test.js
 
-import React  from "react";
-import ReactDom from "react-dom";
-import Calender  from "../index";
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
 
-it('Calender renders without crashing..',()=>{
- const div = document.createElement("div");
- ReactDom.render(<Calender></Calender>,div);
- ReactDom.unmountComponentAtNode(div);
-})
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+jest.mock("axios");
+
+afterEach(cleanup);
+
+it("renders Calender correctly", async () => {
+  const fakeEvents = [
+    {
+      title: "Mock title",
+      start: "1624492800",
+      end: "1624665600",
+      id: "34343444",
+      location: "USA",
+      url: "https:demo/link#3w4/s",
+    },
+  ];
+  axios.get.mockResolvedValue({
+    data: fakeEvents,
+  });
+  const { getByTestId, asFragment } = render(<Calender />);
+});
+
+// it("renders  events data", () => {
+//   const fakeEvents = [
+//     {
+//       title: "Mock title",
+//       start: "1624492800",
+//       end: "1624665600",
+//       id: "34343444",
+//       location: "USA",
+//       url: "https:demo/link#3w4/s",
+//     },
+//   ];
+//   axiosMock.get.mockResolvedValue({ data: fakeEvents });
+//   // Cache original functionality
+//   const realUseState = React.useState;
+
+//   // Stub the initial state
+//   const stubInitialState = fakeEvents;
+
+//   // Mock useState before rendering your component
+//   jest
+//     .spyOn(React, "useState")
+//     .mockImplementationOnce(() => realUseState(stubInitialState));
+
+//   act(() => {
+//     ReactDOM.render(<Calender />, container);
+//   });
+
+//   expect(axiosMock.get).toHaveBeenCalledTimes(1);
+//   expect(axiosMock.get).toHaveBeenCalledWith(
+//     `https://central.wordcamp.org/?rest_route=/wp/v2/wordcamps&_embed&per_page=10&page=${1}`
+//   );
+// });
--- src/components/calender/index.js
+++ src/components/calender/index.js
@@ -1,23 +1,23 @@
 import axios from "axios";
 import moment from "moment";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Calendar, momentLocalizer, Views } from "react-big-calendar";
 import { Spinner } from "react-bootstrap";
 
 function Calender() {
   let allViews = Object.keys(Views).map((k) => Views[k]);
   const localizer = momentLocalizer(moment);
-  const [events, setEvents] = useState([]);
+  const [events, setEvents] = React.useState([]);
+  const [count, setCount] = React.useState(0);
 
   useEffect(() => {
-    const result = axios.get(
-      `https://central.wordcamp.org/?rest_route=/wp/v2/wordcamps&_embed&per_page=10&page=${1}`
-    );
-
-    result.then((res) => {
-      console.log(res.data);
+    const fetchData = async (pagenum = 1) => {
+      const response = await axios.get(
+        `https://central.wordcamp.org/?rest_route=/wp/v2/wordcamps&_embed&per_page=100&page=${pagenum}`
+      );
+      console.log(response);
       let _processedData = [];
-      res.data.map((data) =>
+      response.data.map((data) =>
         _processedData.push({
           title: data.title.rendered,
           start: moment.unix(data["Start Date (YYYY-mm-dd)"]).format("llll"),
@@ -27,52 +27,27 @@ function Calender() {
           url: data.URL,
         })
       );
-      setEvents(_processedData);
-      let count = res.headers["x-wp-totalpages"];
-      count = count / 10;
-      console.log(count);
-      let axiosCalls = [];
-      let processedData = [];
+      let counts = response.headers["x-wp-totalpages"];
+      console.log(counts);
+      setCount(counts);
+      setEvents(events.concat(_processedData));
+    };
 
-      for (let i = 1; i < count; i++) {
-        axiosCalls.push(
-          axios.get(
-            `https://central.wordcamp.org/?rest_route=/wp/v2/wordcamps&_embed&per_page=100&page=${i}`
-          )
-        );
+    fetchData();
+
+    console.log(count);
+    if (count > 0) {
+      let pages = count / 10;
+      console.log(pages);
+      let promises = [];
+      for (let i = 2; i < count; i++) {
+        fetchData(i);
       }
-      axios
-        .all(axiosCalls)
-        .then(
-          axios.spread((...responses) => {
-            console.log(responses.length);
-            responses.map((item) => {
-              item.data.map((data) =>
-                processedData.push({
-                  title: data.title.rendered,
-                  start: moment
-                    .unix(data["Start Date (YYYY-mm-dd)"])
-                    .format("llll"),
-                  end: moment
-                    .unix(data["End Date (YYYY-mm-dd)"])
-                    .format("llll"),
-                  id: data.id,
-                  location: data.location,
-                  url: data.URL,
-                })
-              );
-            });
-            setEvents(processedData);
-            console.log("full data loaded");
-          })
-        )
-        .catch((errors) => {
-          console.log(errors);
-        });
-    });
-  }, []);
+      let res = Promise.all(fetchData);
+    }
+  }, [count]);
   return (
-    <div>
+    <div id="spinner">
       {events && events.length > 0 ? (
         ""
       ) : (
