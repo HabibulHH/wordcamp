221b5f7f702f9146362e2872f54ef86b6176e635
HaibulHasan <habibul.hasan.hira@gmail.com>
2021-09-25 02:07:01 +0600

    Detailed test coverage added with code refactoring

--- src/components/calender/__test__/test.js
+++ src/components/calender/__test__/test.js
@@ -1,19 +1,17 @@
+import { screen } from "@testing-library/dom";
 import { cleanup, render } from "@testing-library/react";
 import axios from "axios";
 import React from "react";
 import { unmountComponentAtNode } from "react-dom";
 import Calender from "../";
-// user.test.js
 
 let container = null;
 beforeEach(() => {
-  // setup a DOM element as a render target
   container = document.createElement("div");
   document.body.appendChild(container);
 });
 
 afterEach(() => {
-  // cleanup on exiting
   unmountComponentAtNode(container);
   container.remove();
   container = null;
@@ -23,7 +21,7 @@ jest.mock("axios");
 
 afterEach(cleanup);
 
-it("renders Calender correctly", async () => {
+it("renders calender correctly", async () => {
   const fakeEvents = [
     {
       title: "Mock title",
@@ -36,39 +34,12 @@ it("renders Calender correctly", async () => {
   ];
   axios.get.mockResolvedValue({
     data: fakeEvents,
+    headers: { "x-wp-totalpages": 11 },
   });
+  const setState = jest.fn();
+  const useStateSpy = jest.spyOn(React, "useState");
+  useStateSpy.mockImplementation((init) => [init, setState]);
   const { getByTestId, asFragment } = render(<Calender />);
+  expect(asFragment()).toMatchSnapshot();
+  expect(screen.getByTestId("spinner"));
 });
-
-// it("renders  events data", () => {
-//   const fakeEvents = [
-//     {
-//       title: "Mock title",
-//       start: "1624492800",
-//       end: "1624665600",
-//       id: "34343444",
-//       location: "USA",
-//       url: "https:demo/link#3w4/s",
-//     },
-//   ];
-//   axiosMock.get.mockResolvedValue({ data: fakeEvents });
-//   // Cache original functionality
-//   const realUseState = React.useState;
-
-//   // Stub the initial state
-//   const stubInitialState = fakeEvents;
-
-//   // Mock useState before rendering your component
-//   jest
-//     .spyOn(React, "useState")
-//     .mockImplementationOnce(() => realUseState(stubInitialState));
-
-//   act(() => {
-//     ReactDOM.render(<Calender />, container);
-//   });
-
-//   expect(axiosMock.get).toHaveBeenCalledTimes(1);
-//   expect(axiosMock.get).toHaveBeenCalledWith(
-//     `https://central.wordcamp.org/?rest_route=/wp/v2/wordcamps&_embed&per_page=10&page=${1}`
-//   );
-// });
--- src/components/calender/index.js
+++ src/components/calender/index.js
@@ -3,22 +3,29 @@ import moment from "moment";
 import React, { useEffect } from "react";
 import { Calendar, momentLocalizer, Views } from "react-big-calendar";
 import { Spinner } from "react-bootstrap";
-
+import { WORD_CAMP_API } from "../../consts";
 function Calender() {
   let allViews = Object.keys(Views).map((k) => Views[k]);
   const localizer = momentLocalizer(moment);
   const [events, setEvents] = React.useState([]);
   const [count, setCount] = React.useState(0);
 
+  const getDataFromWp = async (pagenum = 1) => {
+    const fetchData = async () => {
+      const response = await axios.get(`${WORD_CAMP_API}=${pagenum}`);
+      return response;
+    };
+
+    return fetchData();
+  };
   useEffect(() => {
-    const fetchData = async (pagenum = 1) => {
-      const response = await axios.get(
-        `https://central.wordcamp.org/?rest_route=/wp/v2/wordcamps&_embed&per_page=100&page=${pagenum}`
-      );
-      console.log(response);
-      let _processedData = [];
+    let counts = 0;
+    let _processedData = [];
+    const fetchData = async () => {
+      const response = await getDataFromWp(1);
+      let processedData = [];
       response.data.map((data) =>
-        _processedData.push({
+        processedData.push({
           title: data.title.rendered,
           start: moment.unix(data["Start Date (YYYY-mm-dd)"]).format("llll"),
           end: moment.unix(data["End Date (YYYY-mm-dd)"]).format("llll"),
@@ -27,31 +34,52 @@ function Calender() {
           url: data.URL,
         })
       );
-      let counts = response.headers["x-wp-totalpages"];
-      console.log(counts);
-      setCount(counts);
-      setEvents(events.concat(_processedData));
+      counts = response.headers["x-wp-totalpages"];
+      count == 0 ? setCount(counts) : 0;
+      if (count == 0) setEvents(events.concat(processedData));
+      return response;
     };
 
-    fetchData();
+    if (count == 0) fetchData();
+  }, []);
 
-    console.log(count);
+  useEffect(() => {
+    let processedData = [];
     if (count > 0) {
-      let pages = count / 10;
-      console.log(pages);
-      let promises = [];
-      for (let i = 2; i < count; i++) {
-        fetchData(i);
-      }
-      let res = Promise.all(fetchData);
+      const call_stack = async function () {
+        let promises = [];
+        for (let i = 1; i <= count; i++) {
+          promises.push(getDataFromWp(i));
+        }
+        let results = await Promise.all(promises);
+        return results;
+      };
+      const promisedResult = call_stack();
+      promisedResult.then((results) => {
+        results.map((item) => {
+          item.data.map((data) =>
+            processedData.push({
+              title: data.title.rendered,
+              start: moment
+                .unix(data["Start Date (YYYY-mm-dd)"])
+                .format("llll"),
+              end: moment.unix(data["End Date (YYYY-mm-dd)"]).format("llll"),
+              id: data.id,
+              location: data.location,
+              url: data.URL,
+            })
+          );
+        });
+        setEvents(processedData);
+      });
     }
   }, [count]);
   return (
-    <div id="spinner">
+    <div>
       {events && events.length > 0 ? (
         ""
       ) : (
-        <>
+        <div data-testid="spinner">
           <Spinner animation="grow" variant="secondary" />
           <Spinner animation="grow" variant="success" />
           <Spinner animation="grow" variant="danger" />
@@ -59,7 +87,7 @@ function Calender() {
           <Spinner animation="grow" variant="info" />
           <Spinner animation="grow" variant="light" />
           <Spinner animation="grow" variant="dark" />
-        </>
+        </div>
       )}
 
       <div>
@@ -77,6 +105,7 @@ function Calender() {
       </div>
     </div>
   );
+  return { count, load };
 }
 
 export default Calender;
